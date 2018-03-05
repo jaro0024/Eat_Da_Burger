6961eb6562dc8f6ed554e03677cfdd141f829005
daniela jaros <danielajaros@gmail.com>
2018-03-05 11:20:01 -0500

    created initial files, created the db and inserted data into it and created gitignore file for node modules

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheeseburger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Cheeseburger", false);
