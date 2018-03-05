3047e364773e8a9c08c444965353a531ca519fa7
daniela jaros <danielajaros@gmail.com>
2018-03-05 14:41:05 -0500

    Finished code and worked on the style

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+require("dotenv").config();
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    // My username
+    user: "root",
+    // My password from the dotenv file to keep it secured
+    password: process.env.mysql_pwd,
+    database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+  
+  // Export connection for our ORM to use.
+  module.exports = connection;
