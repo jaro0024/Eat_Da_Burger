7e89bca91eb76d27af3dd8b73d2e1c6dc6fbec13
daniela jaros <danielajaros@gmail.com>
2018-03-07 11:44:50 -0500

    added connection configuration for heroku

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,13 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     // My username
