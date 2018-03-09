52b6f3663aeedb71dd5844003f29086d91d7041a
daniela jaros <danielajaros@gmail.com>
2018-03-09 11:54:28 -0500

    made changes to comments on connection file

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,15 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-//if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 var connection;
-
-
+  // Connecting to JawsDB
   connection = mysql.createConnection({
     host: "yhrz9vns005e0734.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    // My username
     user: "tdyu46zlvk8xh5m5",
-    // My password from the dotenv file to keep it secured
     password: "t45us6fe9rp5pnrl",
     database: "p2ugeemkd1o0bdbc"
 });
-
 
 // Make connection
 connection.connect(function(err) {
