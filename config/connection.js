ac84895644c32ae7d1ba473f4179ec2f284ac289
daniela jaros <danielajaros@gmail.com>
2018-03-12 11:17:58 -0400

    minor changes

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 var connection;
 
@@ -10,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     port: 3306,
     host: "localhost",
     user: "root",
-    password: "", 
+    password: process.env.mysql_pwd, 
     database: "burgers_db"
   });
 }
